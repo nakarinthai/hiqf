8788ed45b41dc8e865ca0a54f700cde60a1fc85b
Nakarin <nakarin@hiqfood.com>
2019-10-09 21:53:12 +0700

    modify

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 #!/usr/bin/env node
 
 const { exec } = require('child_process');
-
-let mkdirStatus = false;
-const appName = process.argv.slice(2)[0] || 'myApp'
+let appName = 'ErrorAppName'
+const template = (process.argv.slice(2) || [])[0] || ''
+let typeTemplate = 'noType'; // client or service
+let templateName = 'noTempName' // template1 or template2
 
 function init() {
+    console.log(process.argv)
 
-    if (appName) {
-        makeDir(callbackMakeDir)
+    if (template === '--help') {
+        console.log('Usage: npx hiqf [type/template] \n  ' +
+            '  -- [type] is mean client or service \n  ' +
+            '  -- [template] is mean pattern template on the repository')
+        return
     }
+
+    if (template) {
+        checkAppName(template)
+        if (appName) {
+            makeDir(callbackMakeDir)
+        }
+    } else {
+        console.log('Please insert client or service \n ' +
+            '[ npx hiqf service/template1 myAppName ] \n ' +
+            '[ npx hiqf client/template1 myAppName ]')
+    }
+
 }
 
-function makeDir(callbackMakeDir) {
+function checkAppName(tempName) {
+    typeTemplate = tempName.split('/')[0] || ''
+    templateName = tempName.split('/')[1] || ''
+    console.log('templateName ', templateName)
+    if (typeTemplate !== '') {
+        appName = (process.argv.slice(2) || [])[1] || 'my-hiq-app'
+    }
+
+}
+
+function makeDir(callbackFunc) {
     const command = `mkdir ${appName} \n cd ${appName} \n npm init --yes \n ls`;
     exec(command, (error, stdout, stderr) => {
-        callbackMakeDir({ error, stdout, stderr })
+        callbackFunc({ error, stdout, stderr })
             .then(resp => resp).catch(err => err)
     })
 }
